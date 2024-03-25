b0128d73d434068d9c5e83efaf3c1b6bd922822b
seokhyunrew <darkknightttt@nave.com>
2024-03-25 18:08:48 +0900

    Feat: "Swagger 기능 구현"
    Swagger 실행만 되게 가장 기본적인 기능 구현, 추가적으로 꾸미거나 추후에 SpringSecurity JWT로 구현하면 SwaggerConfig 부분 바꿔줘야함.

--- src/main/java/community/controller/ArticleController.java
+++ src/main/java/community/controller/ArticleController.java
@@ -2,6 +2,8 @@ package community.controller;
 
 import community.dto.user.ArticleDto;
 import community.service.ArticleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "아티클 CRUD")
 @RestController
 @RequestMapping("/api/articles")
 public class ArticleController {
