cff072dded1ed393aaad96d51fddbc8ec740cf75
SeokhyunRew <darkknightttt@naver.com>
2024-04-02 14:11:45 +0900

    Feat : "유저 권한 코드 수정"

--- src/main/java/community/config/SecurityConfiguration.java
+++ src/main/java/community/config/SecurityConfiguration.java
@@ -1,62 +1,43 @@
 package community.config;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.KeySourceException;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSelector;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-import community.constant.Role;
-import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPublicKey;
-import java.util.List;
-import java.util.UUID;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @EnableWebSecurity
 @Configuration
-public class JwtSecurityConfiguration {
+public class SecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer configure() {      // 스프링 시큐리티 기능 비활성화
         return web -> web.ignoring().requestMatchers(toH2Console())
-                .requestMatchers("/static/**", "/vendor/**", "/css/**", "/img/**", "/js/**", "/swagger-ui/**", "/api-docs/**", "/api/**");
+                .requestMatchers("/static/**", "/vendor/**", "/css/**", "/img/**", "/js/**","/error");
     }
 
     // 특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(auth ->              // 인증, 인가 설정
-                        auth.requestMatchers(HttpMethod.GET, "/article/post").hasRole("USER")
-                                .requestMatchers(HttpMethod.GET, "/login", "/signup", "/", "/article/{id}", "/articles/{type}").permitAll()
+                        auth.requestMatchers(HttpMethod.GET, "/article/post").hasAnyAuthority("USER","ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/login", "/signup", "/", "/article/**", "/articles/{type}").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/user").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/create").hasRole("USER")
-                                .requestMatchers(HttpMethod.PATCH, "/update").hasRole("USER")
-                                .requestMatchers(HttpMethod.DELETE, "/delete").hasRole("USER")
-                                .requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/create").hasAuthority("USER")
+                                .requestMatchers(HttpMethod.PATCH, "/update").hasAuthority("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/delete").hasAuthority("USER")
+                                .requestMatchers(HttpMethod.GET, "/admin").hasAuthority("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .formLogin(auth -> auth.loginPage("/login")     // 폼 기반 로그인 설정
                         .defaultSuccessUrl("/"))
-                .logout(auth -> auth.logoutSuccessUrl("/") // 로그아웃 설정
+                .logout(auth -> auth.logoutUrl("/logout").logoutSuccessUrl("/") // 로그아웃 설정
                         .invalidateHttpSession(true))
                 .csrf(AbstractHttpConfigurer::disable)// csrf 비활성화
                 .sessionManagement(session -> session.maximumSessions(1).maxSessionsPreventsLogin(true).expiredUrl("/login"));
--- src/main/java/community/domain/user/UserEntity.java
+++ src/main/java/community/domain/user/UserEntity.java
@@ -52,7 +52,7 @@ public class UserEntity implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        authorities.add(new SimpleGrantedAuthority(role.name()));
         return authorities;
     }
 
