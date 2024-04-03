9b1e8ee8a53558564968c34f8bdf656e3b067d0b
SeokhyunRew <darkknightttt@naver.com>
2024-04-03 16:43:57 +0900

    Feat : "본인 체크 Interceptor 파일 생성"

---
+++ src/main/java/community/interceptor/AuthInterceptor.java
@@ -0,0 +1,85 @@
+package community.interceptor;
+
+import community.constant.Role;
+import community.domain.user.ArticleEntity;
+import community.exception.UnauthorizedException;
+import community.repository.ArticleRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    //아래서 articleRepository 사용 하려고 선언
+    private final ArticleRepository articleRepository;
+
+    //생성자
+    @Autowired
+    public AuthInterceptor(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+
+            ArticleEntity article = fetchArticle(request);
+
+            if (article != null) {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication != null && authentication.isAuthenticated()) {
+                    // 현재 로그인한 사용자의 정보
+                    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                    // 사용자의 ID 등 필요한 정보를 이용하여 게시물의 작성자와 비교하여 권한 확인
+                    Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+                    for (GrantedAuthority authority : authorities) {
+                        if (authority.getAuthority().equals(Role.ADMIN.name())) {
+                            // 사용자가 ADMIN 권한을 가지고 있으면 true 반환
+                            return true;
+                        }
+                    }
+                    if (!article.getUser().getUsername().equals(userDetails.getUsername())) {
+                        // 권한이 없는 경우
+                        response.setContentType("text/html;charset=UTF-8");
+                        PrintWriter out = response.getWriter();
+                        out.println("<script>alert('해당 글의 작성자만 수정할 수 있습니다.');history.go(-1);</script>");
+                        out.flush();
+                        return false; // 요청 처리 중지
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    private ArticleEntity fetchArticle(HttpServletRequest request) {
+        String idParameter = request.getParameter("id");
+        if (idParameter != null) {
+            try {
+                Long articleId = Long.parseLong(idParameter);
+                return articleRepository.findById(articleId).orElse(null);
+            } catch (NumberFormatException e) {
+                // 파라미터를 Long으로 파싱할 수 없는 경우
+                return null;
+            }
+        }
+        return null;
+    }
+
+}
