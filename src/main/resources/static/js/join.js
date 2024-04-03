5821dc8ebda2f384d791d0cde99655115cb79c95
BlueHarmel <dldjdtjr@naver.com>
2024-04-03 16:19:56 +0900

    Fix: "검증 함수간 비동기 문제 해결"
    검증 함수간에 동기화 문제를 해결

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,3 +1,79 @@
+// let password = document.getElementById('password');
+// let password2 = document.getElementById('password2');
+// let emailInput = document.getElementById('id');
+// let nicknameInput = document.getElementById('nickName');
+//
+// function checkPassword() {
+//     if (password.value !== password2.value) {
+//         alert('비밀번호가 동일하지 않습니다.');
+//         password.value = "";
+//         password2.value = "";
+//         return false;
+//     }
+//     alert('비밀번호가 동일합니다.');
+//     return true;
+// }
+//
+// function checkEmail() {
+//     const email = emailInput.value;
+//
+//     if (!email) {
+//         alert("이메일을 입력해주세요.");
+//         return false;
+//     }
+//
+//     fetch('/checkEmail', {
+//         method: 'POST',
+//         body: JSON.stringify({email}),
+//         headers: {'Content-Type': 'application/json'}
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 alert("사용 가능한 이메일 입니다.");
+//                 return true;
+//             } else {
+//                 alert("사용중인 이메일 입니다.");
+//                 emailInput.value = "";
+//                 return false;
+//             }
+//         })
+// }
+//
+// function checkNickname() {
+//     const nickName = nicknameInput.value;
+//
+//     if (!nickName) {
+//         alert("닉네임을 입력해주세요.");
+//         return false;
+//     }
+//
+//     fetch('/checkNickName', {
+//         method: 'POST',
+//         body: JSON.stringify({nickName}),
+//         headers: {'Content-Type': 'application/json'}
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 alert("사용 가능한 닉네임 입니다.");
+//                 return true;
+//             } else {
+//                 alert("사용중인 닉네임 입니다.");
+//                 nicknameInput.value = "";
+//                 return false;
+//             }
+//         })
+// }
+//
+// var form = document.getElementById('form');
+//
+// function formSubmit() {
+//     if (checkEmail() && checkNickname() && checkPassword()) {
+//         form.action = "/user";
+//         form.method = "POST";
+//         form.submit();
+//     }
+// }
+
 let password = document.getElementById('password');
 let password2 = document.getElementById('password2');
 let emailInput = document.getElementById('id');
@@ -10,7 +86,6 @@ function checkPassword() {
         password2.value = "";
         return false;
     }
-    alert('비밀번호가 동일합니다.');
     return true;
 }
 
@@ -22,21 +97,20 @@ function checkEmail() {
         return false;
     }
 
-    fetch('/checkEmail', {
+    return fetch('/checkEmail', {
         method: 'POST',
         body: JSON.stringify({email}),
         headers: {'Content-Type': 'application/json'}
     })
         .then(response => {
             if (response.ok) {
-                alert("사용 가능한 이메일 입니다.");
                 return true;
             } else {
                 alert("사용중인 이메일 입니다.");
                 emailInput.value = "";
                 return false;
             }
-        })
+        });
 }
 
 function checkNickname() {
@@ -47,29 +121,30 @@ function checkNickname() {
         return false;
     }
 
-    fetch('/checkNickName', {
+    return fetch('/checkNickName', {
         method: 'POST',
         body: JSON.stringify({nickName}),
         headers: {'Content-Type': 'application/json'}
     })
         .then(response => {
             if (response.ok) {
-                alert("사용 가능한 닉네임 입니다.");
                 return true;
             } else {
                 alert("사용중인 닉네임 입니다.");
                 nicknameInput.value = "";
                 return false;
             }
-        })
+        });
 }
 
 var form = document.getElementById('form');
 
 function formSubmit() {
-    if (checkEmail() && checkNickname() && checkPassword()) {
-        form.action = "/user";
-        form.method = "POST";
-        form.submit();
-    }
+    Promise.all([checkEmail(), checkNickname()]).then(([emailValid, nickNameValid]) => {
+        if (emailValid && nickNameValid && checkPassword()) {
+            form.action = "/user";
+            form.method = "POST";
+            form.submit();
+        }
+    });
 }
