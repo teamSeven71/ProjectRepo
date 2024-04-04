25720bdbec58a0b7ddae9da71ca8a2b84b3c956c
BlueHarmel <dldjdtjr@naver.com>
2024-04-04 09:28:05 +0900

    Feat: "유저정보 페이지 접근"
    메인페이지에서 유저 정보에 접근하는 방법을 구현

--- src/main/java/community/controller/UserViewController.java
+++ src/main/java/community/controller/UserViewController.java
@@ -1,11 +1,14 @@
 package community.controller;
 
 
+import community.domain.user.UserEntity;
 import community.dto.user.UserDto;
 import community.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +39,18 @@ public class UserViewController {
         request.getSession().invalidate();
         return "redirect:/";
     }
+
+    // 유저 정보 페이지
+//    @GetMapping("/userInfo")
+//    public String UserInfo(Model model){
+//        // 현재 로그인한 사용자 정보 가져오기
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        UserEntity userEntity = (UserEntity) authentication.getPrincipal();
+//
+//        UserDto.UserResponseDto userDto = new UserDto.UserResponseDto();
+//        userDto.setUsername()
+//        return null;
+//    }
 
     // 관리자 페이지
     @GetMapping("/admin")
