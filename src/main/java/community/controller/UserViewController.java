6a9d8acc94bcb4426091b9c7c7582783caf0794b
SeokhyunRew <darkknightttt@naver.com>
2024-04-04 13:53:25 +0900

    Feat : "pagination Controller 메소드 수정"

--- src/main/java/community/controller/UserViewController.java
+++ src/main/java/community/controller/UserViewController.java
@@ -1,16 +1,18 @@
 package community.controller;
 
 
-import community.dto.user.UserDto;
+import community.domain.user.UserEntity;
 import community.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 @Controller
 public class UserViewController {
@@ -39,9 +41,16 @@ public class UserViewController {
 
     // 관리자 페이지
     @GetMapping("/admin")
-    public String showMembers(Model model){
-        List<UserDto.UserResponseDto> members = userService.getAllActiveMembers();
+    public String showMembers(@PageableDefault(size=10) Pageable pageable, Model model) {
+        Page<UserEntity> members = userService.getAllActiveMembers(pageable);
+        int startPage = Math.max(1, members.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(members.getPageable().getPageNumber()+4, members.getTotalPages());
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("members", members);
         return "/site/adminPage";
     }
+
+
 }   
