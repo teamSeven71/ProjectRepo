c3301c23a9dd6d6552d774e67b86e5f1d29c86f9
SeokhyunRew <darkknightttt@naver.com>
2024-04-05 09:59:51 +0900

    Feat : "관리자 페이지 user nickName으로 검색 기능 구현"

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -30,3 +30,25 @@ deleteButton.addEventListener('click', ()=> {
             console.error('삭제 실패:', error);
         });
 });
+
+function searchContent() {
+    event.preventDefault(); // 기본 이벤트 동작 방지
+
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.querySelector(".table_user");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1]; // Change the index according to the column you want to search
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
